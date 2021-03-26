3fe5d9b910eebeb7649d65feceb8b4b373f67342
Chew Choong <ccchen1213@gmail.com>
2021-03-26 23:34:01 +0800

    clean up and remove not necessary code

--- src/app/services/excel-upload.service.ts
+++ src/app/services/excel-upload.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Employee } from '../interfaces/employee';
 
@@ -7,17 +7,12 @@ import { Employee } from '../interfaces/employee';
   providedIn: 'root'
 })
 export class ExcelUploadService {
-  httpHeader = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json'
-    })
-  };
 
   constructor(private http: HttpClient) {}
 
-  upload(file: File): Observable<Employee[]>{
+  public upload(file: File): Observable<Employee[]>{
     const formData = new FormData();
-    formData.append(file.name, file);
+    formData.append('file', file, file.name);
     return this.http.post<Employee[]>('https://localhost:5001/api/Employee', formData);
   }
 }
